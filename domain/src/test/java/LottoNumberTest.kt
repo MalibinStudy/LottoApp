0a6acd38086d9e9302a44564d666b7001e441608
ChunBaee <mljung10@naver.com>
2023-01-26 15:59:24 +0900

    feat: LottoNumber 1차 TC 작성

--- domain/src/test/java/LottoNumberTest.kt
+++ domain/src/test/java/LottoNumberTest.kt
@@ -7,6 +7,17 @@ import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
 class LottoNumberTest {
+    @ValueSource(ints = [1,2,3,4,5,45])
+    @ParameterizedTest
+    fun `LOTTO_NUMBER가_정상일_때_잘_동작하는지?`(number : Int) {
+        val actualNumber = LottoNumber.of(number)
+        assertThat(actualNumber).isEqualTo(LottoNumber.of(number))
+    }
 
-
+    @ValueSource(ints = [0,-2,63,47,53,46])
+    @ParameterizedTest
+    fun `LOTTO_NUMBER가_비정상일_때_잘_동작하는지?`(number : Int) {
+        val exception = kotlin.runCatching { LottoNumber.of(number) }.exceptionOrNull()
+        assertThat(exception).isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
