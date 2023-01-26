40b6652c4a690418612880a3f074ab2f920f4188
murjune <sis9221302@gmail.com>
2023-01-27 03:06:57 +0900

    [step1] LottoNumber Unit Test

--- domain/src/test/java/com/malibin/study/domain/lotto/LottoNumberTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/LottoNumberTest.kt
@@ -1,5 +1,27 @@
 package com.malibin.study.domain.lotto
 
-import org.junit.jupiter.api.Assertions.*
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
-internal class LottoNumberTest
+internal class LottoNumberTest {
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 45])
+    fun `45이하의 자연수로 LottoNumber 객체를 가져올 수 있다`(num: Int) {
+        // when
+        val actualLottoNumber = LottoNumber.of(num)
+        // then
+        assertThat(actualLottoNumber.number).isEqualTo(num)
+    }
+
+    @Test
+    fun `45이하의 자연수가 아닌 수로 LottoNumber 객체를 가져올 수없다`() {
+        // given
+        val invalidNum = 0
+        // when
+        val actualException = runCatching { LottoNumber.of(invalidNum) }.exceptionOrNull()
+        // then
+        assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java)
+    }
+}
