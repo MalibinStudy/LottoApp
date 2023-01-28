350588f9179032aa4e8dfab8adfe960228df4740
stopkite <stopkite98@naver.com>
2023-01-28 11:54:39 +0900

    test:[Money]연산자 +,-를 이용한 정상 결과값 확인 테스트 코드 추가

--- domain/src/test/java/com/malibin/study/domain/lotto/result/MoneyTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/result/MoneyTest.kt
@@ -4,6 +4,7 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 internal class MoneyTest {
@@ -32,5 +33,23 @@ internal class MoneyTest {
                     .contains("돈의 액수는 음수가 될 수 없습니다. 입력 값 : $negativeAmount")
             }
         )
+    }
+
+    @Test
+    fun `Money를 연산자 '+'로 더할 수 있다`() {
+        // when
+        val actualMoney = Money(1_000) + Money(2_000)
+
+        // then
+        assertThat(actualMoney).isEqualTo(Money(3_000))
+    }
+
+    @Test
+    fun `Money를 연산자 '-'로 뺄 수 있다`() {
+        // when
+        val actualMoney = Money(3_000) - Money(2_000)
+
+        // then
+        assertThat(actualMoney).isEqualTo(Money(1_000))
     }
 }
