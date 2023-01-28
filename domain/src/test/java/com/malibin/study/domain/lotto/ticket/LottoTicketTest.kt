d8a5b0b6b123a4a09cfcf7c52da0499627e2cd7e
stopkite <stopkite98@naver.com>
2023-01-28 12:07:00 +0900

    test:[LottoTicket]로또 번호가 6개 보다 크거나 작을 때 예외처리 하는 코드 추가

--- domain/src/test/java/com/malibin/study/domain/lotto/ticket/LottoTicketTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/ticket/LottoTicketTest.kt
@@ -18,4 +18,27 @@ internal class LottoTicketTest {
         assertThat(lottoTicket.lottoNumbers.count()).isEqualTo(6)
     }
 
+    @Test
+    fun `로또 번호의 개수가 6보다 크면 예외처리를 던진다`() {
+        // given
+        val lottoNumbers = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+
+        // when
+        val actualException = runCatching { LottoTicket(*lottoNumbers) }.exceptionOrNull()
+
+        // then
+        assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `로또 번호의 개수가 6보다 작으면 예외처리를 던진다`() {
+        // given
+        val lottoNumbers = intArrayOf(1, 2, 3, 4, 5)
+
+        // when
+        val actualException = runCatching { LottoTicket(*lottoNumbers) }.exceptionOrNull()
+
+        // then
+        assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
