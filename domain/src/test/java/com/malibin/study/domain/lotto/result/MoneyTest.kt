3174314882aa3e76602025ce10d0c8680cbd0bd9
이빵주 <yungju9982@naver.com>
2023-01-28 13:42:54 +0900

    돈의 액수가 음수일 경우 테스트 코드 작성

--- domain/src/test/java/com/malibin/study/domain/lotto/result/MoneyTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/result/MoneyTest.kt
@@ -1,4 +1,20 @@
 package com.malibin.study.domain.lotto.result
 
+import com.google.common.truth.Truth
+import org.junit.jupiter.api.Test
+
 internal class MoneyTest {
+    @Test
+    fun `돈의 액수는 음수가 될 수 없다`() {
+        // given
+        val money = Money(-1)
+
+        // when
+        val exception = kotlin.runCatching {
+            money
+        }.exceptionOrNull()
+
+        // then
+        Truth.assertThat(exception).isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
