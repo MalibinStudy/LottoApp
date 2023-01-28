00755cfecda7715a91c863e8288520f0548d2d34
stopkite <stopkite98@naver.com>
2023-01-28 11:49:58 +0900

    test:[Money]amount에 0이상의 값이 들어갈 시 정상 출력 확인 테스트 코드 추가

---
+++ domain/src/test/java/com/malibin/study/domain/lotto/result/MoneyTest.kt
@@ -0,0 +1,36 @@
+package com.malibin.study.domain.lotto.result
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class MoneyTest {
+
+    @ValueSource(longs = [0, 1, 1000, 10000])
+    @ParameterizedTest
+    fun `amount에 0이상의 값이 들어올 수 있다`(positiveAmount: Long) {
+        // given
+        val actualAmount = Money(positiveAmount).amount
+
+        // then
+        assertThat(actualAmount).isAtLeast(0)
+    }
+
+    @ValueSource(longs = [-10, -5, -1, -4])
+    @ParameterizedTest
+    fun `amount에 음수가 들어오면 예외를 던진다`(negativeAmount: Long) {
+        // given
+        val actualException = runCatching { Money(negativeAmount).amount }.exceptionOrNull()
+
+        // then
+        assertAll(
+            { assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java) },
+            {
+                assertThat(actualException).hasMessageThat()
+                    .contains("돈의 액수는 음수가 될 수 없습니다. 입력 값 : $negativeAmount")
+            }
+        )
+    }
+}
