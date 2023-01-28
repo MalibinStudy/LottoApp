d991ec2796e7545c348837c461425b5b69044e58
ChunBaee <mljung10@naver.com>
2023-01-28 17:49:17 +0900

    docs: 2단계 기능목록 - WinningTicketTest 작성

---
+++ domain/src/test/java/com/malibin/study/domain/lotto/ticket/WinningTicketTest.kt
@@ -0,0 +1,11 @@
+package com.malibin.study.domain.lotto.ticket
+
+import com.google.common.truth.Truth.assertThat
+import com.malibin.study.domain.lotto.LottoNumber
+import org.junit.jupiter.api.Test
+
+class WinningTicketTest {
+
+
+
+}
