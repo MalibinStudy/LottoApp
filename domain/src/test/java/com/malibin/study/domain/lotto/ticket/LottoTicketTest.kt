799b05fda063b3b9033978404dcb942e9d79ad95
stopkite <stopkite98@naver.com>
2023-01-28 12:56:37 +0900

    test:[LottoTicket]일치하는 번호의 존재여부를 확인하는 테스트 코드 추가

--- domain/src/test/java/com/malibin/study/domain/lotto/ticket/LottoTicketTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/ticket/LottoTicketTest.kt
@@ -1,8 +1,13 @@
 package com.malibin.study.domain.lotto.ticket
 
 import com.google.common.truth.Truth.assertThat
+import com.malibin.study.domain.lotto.LottoNumber
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
 
 internal class LottoTicketTest {
 
@@ -41,4 +46,38 @@ internal class LottoTicketTest {
         // then
         assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java)
     }
+
+    @ParameterizedTest
+    @MethodSource("providesLottoNumbers")
+    fun `로또 티켓에 일치 하는 숫자가 존재 하는 지 확인할 수 있다`(lottoNumbers: IntArray) {
+        // given
+        val lottoTicket = LottoTicket(*lottoNumbers)
+        val lottoNumber = LottoNumber.of(1)
+
+        // then
+        assertThat(lottoTicket.has(lottoNumber)).isTrue()
+    }
+
+    @ParameterizedTest
+    @MethodSource("providesLottoNumbers")
+    fun `로또 티켓에 일치 하는 숫자가 하나도 없는 지 확인할 수 있다`(lottoNumbers: IntArray) {
+        // given
+        val lottoTicket = LottoTicket(*lottoNumbers)
+        val lottoNumber = LottoNumber.of(7)
+
+        // then
+        assertThat(lottoTicket.has(lottoNumber)).isFalse()
+    }
+
+    companion object {
+        @JvmStatic
+        fun providesLottoNumbers(): List<Arguments> =
+            listOf(
+                Arguments.of(intArrayOf(1, 2, 3, 4, 5, 6)),
+                Arguments.of(intArrayOf(1, 10, 20, 30, 40, 45)),
+                Arguments.of(intArrayOf(1, 12, 23, 24, 35, 42))
+            )
+    }
+
+
 }
