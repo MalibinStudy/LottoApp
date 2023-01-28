a688262271e5fc7345d9cf3592520fa530533e1e
stopkite <stopkite98@naver.com>
2023-01-28 12:00:20 +0900

    test:[LottoNumber]로또 번호가 1과 45 사이 이외의 수로 주어졌을 때 예외처리 하는 코드추가

--- domain/src/test/java/com/malibin/study/domain/lotto/LottoNumberTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/LottoNumberTest.kt
@@ -14,4 +14,20 @@ internal class LottoNumberTest {
         assertThat(validLottoNumber).isIn(IntRange(1, 45))
     }
 
+    @ValueSource(ints = [-1, 0, 46])
+    @ParameterizedTest
+    fun `로또 번호에 1과 45 사이 이외의 수가 들어오면 예외처리를 던진다`(invalidLottoNumber: Int) {
+        // when
+        val actualException = runCatching { LottoNumber.of(invalidLottoNumber) }.exceptionOrNull()
+
+        // then
+        assertAll(
+            { assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java) },
+            {
+                assertThat(actualException).hasMessageThat()
+                    .contains("입력 숫자 ($invalidLottoNumber)는 로또 숫자 범위(1 ~ 45) 내의 숫자가 아닙니다.")
+            }
+        )
+    }
+
 }
