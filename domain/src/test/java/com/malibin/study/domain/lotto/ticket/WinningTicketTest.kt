f09471cbefcef405c659039002ca5a752009007e
stopkite <stopkite98@naver.com>
2023-01-29 15:03:33 +0900

    test:[WinningTicket]로또 티켓의 당첨 결과를 확인할 수 있는 테스트 코드 추가

--- domain/src/test/java/com/malibin/study/domain/lotto/ticket/WinningTicketTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/ticket/WinningTicketTest.kt
@@ -2,8 +2,12 @@ package com.malibin.study.domain.lotto.ticket
 
 import com.google.common.truth.Truth.assertThat
 import com.malibin.study.domain.lotto.LottoNumber
+import com.malibin.study.domain.lotto.result.Prize
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class WinningTicketTest {
     @Test
@@ -25,4 +29,35 @@ internal class WinningTicketTest {
             }
         )
     }
+
+    @MethodSource("providesTicketsAndResult")
+    @ParameterizedTest
+    fun `로또 티켓의 당첨 결과를 알 수 있다`(lottoTicket: LottoTicket, expectedPrize: Prize) {
+        // given
+        val winningTicket = WinningTicket(
+            LottoTicket(1, 2, 3, 4, 5, 6),
+            LottoNumber.of(10),
+        )
+        // when
+        val actualPrize = winningTicket.compareWith(lottoTicket)
+
+        // then
+        assertThat(actualPrize).isEqualTo(expectedPrize)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun providesTicketsAndResult() = arrayOf(
+            Arguments.of(LottoTicket(1, 2, 3, 4, 5, 6), Prize.First),
+            Arguments.of(LottoTicket(1, 2, 3, 4, 5, 10), Prize.Second),
+            Arguments.of(LottoTicket(1, 2, 3, 4, 5, 15), Prize.Third),
+            Arguments.of(LottoTicket(1, 2, 3, 4, 15, 20), Prize.Fourth),
+            Arguments.of(LottoTicket(1, 2, 3, 15, 20, 25), Prize.Fifth),
+            Arguments.of(LottoTicket(1, 2, 15, 20, 25, 30), Prize.Lose),
+            Arguments.of(LottoTicket(1, 15, 20, 25, 30, 35), Prize.Lose),
+            Arguments.of(LottoTicket(15, 20, 25, 30, 35, 40), Prize.Lose),
+        )
+    }
+
+
 }
