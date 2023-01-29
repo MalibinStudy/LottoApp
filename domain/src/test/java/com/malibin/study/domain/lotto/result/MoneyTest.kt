6fd157653325f7ab8199be26e53fabbf99dcfe45
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-01-30 04:03:05 +0900

    [Feat] Money 테스트 코드 구현
    - amount에 음수가 들어오면 안된다
    - plus 연산이 올바르게 동작해야 한다
    - minus 연산이 올바르게 동작해야 한다

---
+++ domain/src/test/java/com/malibin/study/domain/lotto/result/MoneyTest.kt
@@ -0,0 +1,45 @@
+package com.malibin.study.domain.lotto.result
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+internal class MoneyTest {
+    @Test
+    fun `amount에 음수가 들어오면 안된다`() {
+        //given
+        val amount = -1L
+        //when
+        val actualException = runCatching { Money(amount) }.exceptionOrNull()
+        //then
+        assertAll(
+            { assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java) },
+            {
+                assertThat(actualException).hasMessageThat()
+                    .contains("돈의 액수는 음수가 될 수 없습니다. 입력 값 : $amount")
+            }
+        )
+    }
+
+    @Test
+    fun `plus 연산이 올바르게 동작해야 한다`() {
+        //given
+        val a = 3L
+        val b = 2L
+        //when
+        val actualPlus = Money(a) + Money(b)
+        //then
+        assertThat(actualPlus).isEqualTo(Money(5L))
+    }
+
+    @Test
+    fun `minus 연산이 올바르게 동작해야 한다`() {
+        //given
+        val a = 3L
+        val b = 2L
+        //when
+        val actualMinus = Money(a) - Money(b)
+        //then
+        assertThat(actualMinus).isEqualTo(Money(1L))
+    }
+}
