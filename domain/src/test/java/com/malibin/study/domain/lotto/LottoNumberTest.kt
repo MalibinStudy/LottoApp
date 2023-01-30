ad1f9bfe1c9a098299d542d8c242a66693f1bef9
이빵주 <yungju9982@naver.com>
2023-01-31 03:02:53 +0900

    LottoNumberTest : 입력 숫자가 1보다 작은 경우 테스트 코드 구현

--- domain/src/test/java/com/malibin/study/domain/lotto/LottoNumberTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/LottoNumberTest.kt
@@ -1,3 +1,20 @@
 package com.malibin.study.domain.lotto
 
-internal class LottoNumberTest
+import com.google.common.truth.Truth
+import org.junit.jupiter.api.Test
+
+internal class LottoNumberTest {
+    @Test
+    fun `입력 숫자는 1보다 작을 수 없다`() {
+        // given
+        val number = 0
+
+        // when
+        val exception = kotlin.runCatching {
+            LottoNumber.of(number)
+        }.exceptionOrNull()
+
+        // then
+        Truth.assertThat(exception).isInstanceOf(IllegalArgumentException::class.java)
+    }
+}
