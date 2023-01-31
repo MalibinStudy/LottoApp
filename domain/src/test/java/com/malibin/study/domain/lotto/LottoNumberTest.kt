3d9d222f872c19857ccd07435269c1060c73b539
murjune <sis9221302@gmail.com>
2023-01-31 17:30:22 +0900

    [fix] 코드리뷰 반영

--- domain/src/test/java/com/malibin/study/domain/lotto/LottoNumberTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/LottoNumberTest.kt
@@ -1,7 +1,7 @@
 package com.malibin.study.domain.lotto
 
 import com.google.common.truth.Truth.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -15,13 +15,15 @@ internal class LottoNumberTest {
         assertThat(actualLottoNumber.number).isEqualTo(num)
     }
 
-    @Test
-    fun `45이하의 자연수가 아닌 수로 LottoNumber 객체를 가져올 수없다`() {
-        // given
-        val invalidNum = 0
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `45이하의 자연수가 아닌 수로 LottoNumber 객체를 가져올 수 없다`(inValidNum: Int) {
         // when
-        val actualException = runCatching { LottoNumber.of(invalidNum) }.exceptionOrNull()
+        val actualException = runCatching { LottoNumber.of(inValidNum) }.exceptionOrNull()
         // then
-        assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java)
+        assertAll(
+            { assertThat(actualException).isInstanceOf(IllegalArgumentException::class.java) },
+            { assertThat(actualException?.message).isEqualTo("입력 숫자 ($inValidNum)는 로또 숫자 범위(1 ~ 45) 내의 숫자가 아닙니다.") }
+        )
     }
 }
--- domain/src/test/java/com/malibin/study/domain/lotto/ticket/LottoTicketTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/ticket/LottoTicketTest.kt
@@ -3,6 +3,7 @@ package com.malibin.study.domain.lotto.ticket
 import com.google.common.truth.Truth.assertThat
 import com.malibin.study.domain.lotto.LottoNumber
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -22,50 +23,103 @@ internal class LottoTicketTest {
     @Test
     fun `LottoTicket은 크기가 6개의 LottoNumber Set으로 만들 수 있다`() {
         // given
-        val lottoNums = (1..6).map { LottoNumber.of(it) }.toSet()
+        val lottoNums = setOf(
+            LottoNumber.of(1),
+            LottoNumber.of(2),
+            LottoNumber.of(3),
+            LottoNumber.of(4),
+            LottoNumber.of(5),
+            LottoNumber.of(6)
+        )
         // when
         val actualLottoTicket = LottoTicket(lottoNums)
         // then
-        // 이렇게 당위성을 체크하는게 맞을까요..?
         assertThat(actualLottoTicket.lottoNumbers.size).isEqualTo(lottoNums.size)
     }
 
     @Test
     fun `LottoTicket은 크기가 6개의 LottoNumber List로 만들 수 있다`() {
         // given
-        val lottoNums = (1..6).map { LottoNumber.of(it) }
+        val lottoNumbers = listOf(
+            LottoNumber.of(1),
+            LottoNumber.of(2),
+            LottoNumber.of(3),
+            LottoNumber.of(4),
+            LottoNumber.of(5),
+            LottoNumber.of(6)
+        )
         // when
-        val actualLottoTicket = LottoTicket(lottoNums)
+        val actualLottoTicket = LottoTicket(lottoNumbers)
         // then
         // 이렇게 당위성을 체크하는게 맞을까요..?
-        assertThat(actualLottoTicket.lottoNumbers.size).isEqualTo(lottoNums.size)
+        assertThat(actualLottoTicket.lottoNumbers.size).isEqualTo(lottoNumbers.size)
     }
 
     @Test
     fun `LottoNumber가 6개가 아니면 LottoTicket을 만들 수 없다`() {
         // given
-        val lottoNumbers = (1..7).map { LottoNumber.of(it) }
+        val invalidLottoNumbers = listOf(
+            LottoNumber.of(1),
+            LottoNumber.of(2),
+            LottoNumber.of(3),
+            LottoNumber.of(4),
+            LottoNumber.of(5),
+            LottoNumber.of(6),
+            LottoNumber.of(7)
+        )
         // when
-        val exception = runCatching { LottoTicket(lottoNumbers) }.getOrNull()
+        val exception = runCatching { LottoTicket(invalidLottoNumbers) }.exceptionOrNull()
         // then
-        assertThat(exception).isNull()
+        assertAll(
+            { assertThat(exception).isInstanceOf(IllegalArgumentException::class.java) },
+            { assertThat(exception?.message).isEqualTo("로또 티켓에 번호는 6개만 넣을 수 있습니다. 입력 값 : ${invalidLottoNumbers.toSet()}") }
+        )
     }
 
     @Test
     fun `중복된 LottoNumber가 포함된 크기가 6개의 LottoNumber 리스트로는 LottoTicket을 만들 수 없다`() {
         // given
-        val lottoNumbers = intArrayOf(1, 3, 3, 2, 21, 5).map { LottoNumber.of(it) }
+        val lottoNumbers = listOf(
+            LottoNumber.of(1),
+            LottoNumber.of(3),
+            LottoNumber.of(3),
+            LottoNumber.of(2),
+            LottoNumber.of(21),
+            LottoNumber.of(5)
+        )
         // when
-        val exception = runCatching { LottoTicket(lottoNumbers) }.getOrNull()
+        val exception = runCatching { LottoTicket(lottoNumbers) }.exceptionOrNull()
         // then
-        assertThat(exception).isNull()
+        assertAll(
+            { assertThat(exception).isInstanceOf(IllegalArgumentException::class.java) },
+            {
+                assertThat(exception?.message).isEqualTo(
+                    "로또 티켓에 번호는 6개만 넣을 수 있습니다. 입력 값 : ${lottoNumbers.toSet()}"
+                )
+            }
+        )
     }
 
     @Test
-    fun `countMatchingNumbers() 는 LottoTicket의 LottoNumber와 다른 LottoTicket의 LottoNumber를 비교하여 일치하는 숫자의 개수를 반환한다`() {
+    fun `countMatchingNumbers() 는 2개의 LottoTicket의 공통된 숫자 개수를 반환한다`() {
         // given
-        val lottoNumbers1 = (1..6).map { LottoNumber.of(it) }
-        val lottoNumbers2 = intArrayOf(3, 4, 5, 6, 7, 8).map { LottoNumber.of(it) }
+        val lottoNumbers1 = setOf(
+            LottoNumber.of(1),
+            LottoNumber.of(1),
+            LottoNumber.of(2),
+            LottoNumber.of(3),
+            LottoNumber.of(4),
+            LottoNumber.of(5),
+            LottoNumber.of(6),
+        )
+        val lottoNumbers2 = setOf(
+            LottoNumber.of(3),
+            LottoNumber.of(4),
+            LottoNumber.of(5),
+            LottoNumber.of(6),
+            LottoNumber.of(7),
+            LottoNumber.of(8),
+        )
         val lottoTicket1 = LottoTicket(lottoNumbers1)
         val lottoTicket2 = LottoTicket(lottoNumbers2)
         // when
@@ -73,11 +127,20 @@ internal class LottoTicketTest {
         // then
         assertThat(actualCount).isEqualTo(4)
     }
+
     @ParameterizedTest
     @CsvSource(value = ["3:true", "33:false"], delimiter = ':')
     fun `has() 는 LottoTicket에 특정 LottoNumber가 포함되어 있는지 확인한다`(num: Int, expected: Boolean) {
         // given
-        val lottoNumbers = (1..6).map { LottoNumber.of(it) }
+        val lottoNumbers = setOf(
+            LottoNumber.of(1),
+            LottoNumber.of(1),
+            LottoNumber.of(2),
+            LottoNumber.of(3),
+            LottoNumber.of(4),
+            LottoNumber.of(5),
+            LottoNumber.of(6),
+        )
         val lottoTicket = LottoTicket(lottoNumbers)
         // when
         val actualIsInclude = lottoTicket.has(LottoNumber.of(num))
