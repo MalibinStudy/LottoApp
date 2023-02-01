a21394a110687bcdfe6b6b256a8be84c4756ff81
stopkite <stopkite98@naver.com>
2023-02-01 17:26:18 +0900

    test: [LottoTicketGenerator] 수동으로 생성한 6개의 로또 티켓이 생성됨을 확인할 수 있다

--- domain/src/test/java/com/malibin/study/domain/lotto/generator/LottoTicketGeneratorTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/generator/LottoTicketGeneratorTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 internal class LottoTicketGeneratorTest {
 
     @Test
-    fun `자동 티켓 생성 시 6개의 랜덤 번호가 오름차순으로 생성된다`() {
+    fun `자동 티켓 생성 시 6개의 랜덤 번호가 생성된다`() {
         // given
         val generatingLottoNumbersStrategy = GeneratingLottoNumbersStrategy {
             setOf(
@@ -34,5 +34,15 @@ internal class LottoTicketGeneratorTest {
     }
 
     @Test
+    fun `수동 티켓 생성 시 6개의 번호가 생성 됨을 확인할 수 있다`() {
+        // given
+        val expectedLottoTicket = LottoTicketGenerator().createManualTicket(setOf(1, 2, 3, 4, 5, 6))
+
+        // when
+        val actualLottoTicket = LottoTicket(1, 2, 3, 4, 5, 6)
+
+        // then
+        Truth.assertThat(actualLottoTicket).isEqualTo(expectedLottoTicket)
+    }
 
 }
