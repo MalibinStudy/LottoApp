23b39549274b912a549f7324433f60dc78d31640
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-02-02 08:25:07 +0900

    [Fix] 2단계 미션 리뷰 반영
    Parameterized Test + CsvSource를 잘 활용해주셨네요! 👍👍
    리뷰 내용 :
    다만, 보너스볼과는 상관 없는 3, 4개 맞은 케이스에 true 항목이 있으면, 특정한 의도를 담았다는 착각을 불러 일으킬지도 모르겠어요.
    2등을 제외한 항목은 모두 false로 변경해보는 것은 어떨까요?

--- domain/src/test/java/com/malibin/study/domain/lotto/result/PrizeTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/result/PrizeTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 internal class PrizeTest {
-    @CsvSource(value = ["6,true,First", "5,true,Second", "5,false,Third", "4,true,Fourth", "3,true,Fifth", "2,false,Lose"])
+    @CsvSource(value = ["6,false,First", "5,true,Second", "5,false,Third", "4,false,Fourth", "3,false,Fifth", "2,false,Lose"])
     @ParameterizedTest
     fun `당첨된 수의 개수와 보너스 숫자에 따른 올바른 등수를 매기는 지를 체크한다`(match: Int, has: Boolean, prize: Prize) {
         //when
