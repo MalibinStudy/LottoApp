1e212cac26b9dd1582feab187233b91dc0e21d5a
murjune <sis9221302@gmail.com>
2023-02-09 17:19:40 +0900

    [fix] 불필요한 Test 케이스 삭제!

--- domain/src/test/java/com/malibin/study/domain/lotto/result/PrizeTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/result/PrizeTest.kt
@@ -9,17 +9,16 @@ internal class PrizeTest {
     @ParameterizedTest
     @CsvSource(
         value = [
-            "0,true, Lose", "0,false,Lose",
-            "1,true,Lose", "1,false,Lose",
-            "2,true,Lose", "2,false,Lose",
-            "3,true,Fifth", "3,false,Fifth",
-            "4,true,Fourth", "4,false,Fourth",
+            "0,false,Lose",
+            "1,false,Lose",
+            "2,false,Lose",
+            "3,false,Fifth",
+            "4,false,Fourth",
             "5,true,Second", "5,false,Third",
-            "6,true,First", "6,false,First",
-            "7,true,Lose", "7,false,Lose",
-            "-1,true,Lose", "-1,false,Lose"
-        ],
-        delimiter = ','
+            "6,false,First",
+            "7,true,Lose",
+            "-1,true,Lose"
+        ]
     )
     fun `로또 번호와 맞는 숫자 개수와 보너스 숫자의 여부에 따라 find()함수를 통해 Prize를 얻을 수 있다`(
         matchedNumberCount: Int,
