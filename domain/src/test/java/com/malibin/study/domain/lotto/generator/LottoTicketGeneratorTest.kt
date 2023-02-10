d2cd0f7a4baf81a0c4933b30a5c39950a4fd6219
murjune <sis9221302@gmail.com>
2023-02-10 19:47:15 +0900

    [test] 직접 로또 넘버를 입력하여 자동 로또 티켓을 만드는 방식을 바꿀 수 있다

--- domain/src/test/java/com/malibin/study/domain/lotto/generator/LottoTicketGeneratorTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/generator/LottoTicketGeneratorTest.kt
@@ -1,6 +1,7 @@
 package com.malibin.study.domain.lotto.generator
 
 import com.google.common.truth.Truth.assertThat
+import com.malibin.study.domain.lotto.LottoNumber
 import com.malibin.study.domain.lotto.ticket.LottoTicket
 import org.junit.jupiter.api.Test
 
@@ -16,5 +17,25 @@ internal class LottoTicketGeneratorTest {
         val actualLottoTicket = lottoTicketGenerator.createManualTicket(nums)
         // then
         assertThat(actualLottoTicket).isEqualTo(LottoTicket(1, 2, 3, 4, 5, 6))
+    }
+
+    @Test
+    fun `직접 로또 넘버를 입력하여 자동 로또 티켓을 만드는 방식을 바꿀 수 있다`() {
+        // given
+        val newGeneratingLottoNumbersStrategy = GeneratingLottoNumbersStrategy {
+            setOf(
+                LottoNumber.of(1),
+                LottoNumber.of(2),
+                LottoNumber.of(3),
+                LottoNumber.of(4),
+                LottoNumber.of(5),
+                LottoNumber.of(6)
+            )
+        }
+        val lottoTicketGenerator = LottoTicketGenerator(newGeneratingLottoNumbersStrategy)
+        // when
+        val actualAutoLottoTicket = lottoTicketGenerator.createAutoTicket()
+        // then
+        assertThat(actualAutoLottoTicket).isEqualTo(LottoTicket(1, 2, 3, 4, 5, 6))
     }
 }
